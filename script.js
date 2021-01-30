9354d754cc09de50ae0e0697e333311cd07efe1f
Tahmid Chowdhury <mmhtc1698@gmail.com>
2021-01-30 13:09:18 +0600

    Done 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function eventChange(inputID, eventIncrease) {
-    const inputNumber=getInputValue(inputID);
-    let finalInput=inputNumber;
+    const inputNumber = getInputValue(inputID);
+    let finalInput = inputNumber;
 
 
     if (eventIncrease == false && finalInput > 0) {
-         finalInput = inputNumber - 1;
-    } if(eventIncrease == true) {
-         finalInput = inputNumber + 1;
+        finalInput = inputNumber - 1;
+    } if (eventIncrease == true) {
+        finalInput = inputNumber + 1;
     }
 
     document.getElementById(inputID).value = finalInput;
@@ -14,29 +14,29 @@ function eventChange(inputID, eventIncrease) {
 }
 
 function calculateTotal() {
-    const firstClassCount =  getInputValue("first-class-value");
-    const economyClassCount= getInputValue("economy-class-value");
+    const firstClassCount = getInputValue("first-class-value");
+    const economyClassCount = getInputValue("economy-class-value");
 
-    const subtotalCount = firstClassCount*150 + economyClassCount*100;
-    document.getElementById('subtotal').innerText=subtotalCount;
+    const subtotalCount = firstClassCount * 150 + economyClassCount * 100;
+    document.getElementById('subtotal').innerText = subtotalCount;
 
-    const vatCount =Math.round(subtotalCount*.1);
-    document.getElementById('vat').innerText=vatCount;
+    const vatCount = Math.round(subtotalCount * .1);
+    document.getElementById('vat').innerText = vatCount;
 
-    const totalCount= subtotalCount+vatCount;
-    document.getElementById('total').innerText=totalCount;   
+    const totalCount = subtotalCount + vatCount;
+    document.getElementById('total').innerText = totalCount;
 }
 
 function getInputValue(category) {
     const inputValue = document.getElementById(category).value;
-    const inputNumber = parseInt(inputValue);  
+    const inputNumber = parseInt(inputValue);
     return inputNumber
 }
 
-const bookNowButton=document.getElementById('book-now-button');
-bookNowButton.addEventListener('click',function () {
-    const bookingArea=document.getElementById('booking-area');
-    bookingArea.style.display='none';
-    const confirmationArea= document.getElementById('confirmation');
-    confirmationArea.style.display="block";
+const bookNowButton = document.getElementById('book-now-button');
+bookNowButton.addEventListener('click', function () {
+    const bookingArea = document.getElementById('booking-area');
+    bookingArea.style.display = 'none';
+    const confirmationArea = document.getElementById('confirmation');
+    confirmationArea.style.display = "block";
 })
